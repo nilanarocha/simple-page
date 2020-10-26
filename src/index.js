371a8eba29284500783801c446e9c6f2e0401c30
Nilana Rocha <nilanaarquitetura@gmail.com>
2020-10-26 16:47:33 +1100

    creating news components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import HomePage from './pages/HomePage';
 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+import MenuBar from '../components/MenuBar';
+import Footer from '../components/Footer';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <Fragment>
+      <MenuBar />
+      <h1>Nilana</h1>
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default HomePage;
