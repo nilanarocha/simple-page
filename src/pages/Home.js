62930f236118b6c7397ae94fa7e5366bf8535c52
Nilana Rocha <nilanaarquitetura@gmail.com>
2020-10-26 14:49:51 +1100

    first commint

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React {Fragment} from 'react';
+import './Home.css';
+
+
+export default Home {
+
+  return(){
+    <Fragment>
+      <Head></Head>
+      <Body></Body>
+      <Footer></Footer>
+    </Fragment>
+  }
+
+
+}
